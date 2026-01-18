99cb8429bed57cdb5b7e1c674165167bb6acf7ca
kooroosh1363 <peymanraad1363@gmail.com>
2026-01-18 22:05:08 +0400

    agent

--- src/d2t_agent/org.py
+++ src/d2t_agent/org.py
@@ -1,4 +1,8 @@
 from ai_agents_libs import *
+from ai_agents_libs import *
+
+results = col.query(query_texts=["What did agent learn?"], n_results=1)
+print(results)
 
 agent = Agent(
     role="Searcher",
