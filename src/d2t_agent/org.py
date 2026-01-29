0026f4b178279c20208482c188e8a2851be3fb20
kooroosh1363 <peymanraad1363@gmail.com>
2026-01-29 17:51:17 +0400

    code 7

--- src/d2t_agent/org.py
+++ src/d2t_agent/org.py
@@ -2,6 +2,13 @@ from ai_agents_libs import *
 
 # config.py
 import os
+# agent.py
+class SimpleAgent:
+    def init(self, name):
+        self.name = name
+
+    def act(self, task):
+        return f"{self.name} is working on: {task}"
 from dotenv import load_dotenv
 
 load_dotenv()
