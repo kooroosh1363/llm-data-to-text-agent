26869fdcec4372b631e1d98b87fd9f62e212aa1c
kooroosh1363 <peymanraad1363@gmail.com>
2026-01-30 21:56:31 +0400

    code 8

--- src/d2t_agent/org.py
+++ src/d2t_agent/org.py
@@ -1,5 +1,11 @@
 from ai_agents_libs import *
+# runner.py
+from agent import SimpleAgent
 
+agent = SimpleAgent("CoreAgent")
+result = agent.act("Analyze data")
+
+print(result)
 import os
 # agent.py
 class SimpleAgent:
